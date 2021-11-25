4fed81dcd1909398fca4a4c9499b2bd252405c36
Hecatomb <60758685+pedroelbanquero@users.noreply.github.com>
2021-11-25 10:01:12 +0100

    Update txt.py

--- modules/txt.py
+++ modules/txt.py
@@ -1,1 +1,10 @@
 
+
+
+inicio = "Baphomet Bot is a comprehensive application of Kabala, solve your questions with the most mothern mechanism to use the Holy writings  "
+
+presentacion="Im Baphomet, your Lord. Choose a option"
+
+
+
+
