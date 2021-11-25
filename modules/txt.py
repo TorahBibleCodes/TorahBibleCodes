72d4060d2302fe907d5c7cae322f082d5f54dec5
W4ikiky <37372254+W4ikiky@users.noreply.github.com>
2021-11-25 11:25:34 +0100

    Update txt.py

--- modules/txt.py
+++ modules/txt.py
@@ -3,7 +3,7 @@
 
 inicio = "Baphomet Bot is a comprehensive application of Kabala, solve your questions with the most modern mechanism using Holy writings"
 
-presentacion="Im Baphomet, your Lord. I'm waiting you to choose an option"
+presentacion= "Im Baphomet, your Lord. I'm waiting you to choose an option"
 
 
 search= "Introduce the term to search inside the kabala"
