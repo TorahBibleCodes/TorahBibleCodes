a3f7e4ecfb8af8f1d19804f252741c1eaa4986e6
Hecatomb <60758685+pedroelbanquero@users.noreply.github.com>
2021-11-25 10:34:05 +0100

    Create imports.py

---
+++ modules/imports.py
@@ -0,0 +1,9 @@
+import logging
+import telegram
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove,KeyboardButton)
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,CallbackQueryHandler,ConversationHandler)
+import os
+import modules.config
+import modules.main
+import pyqrcode
