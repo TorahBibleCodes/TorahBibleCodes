55ca4f1e17b802dbb04304d9eabf75630da9943d
citrixrep <maxtvstream@live.com>
2021-11-25 06:55:50 +0100

    Add install script

---
+++ install.sh
@@ -0,0 +1,2 @@
+apt install python3 python3-pip
+pip3 install -r requirements.txt
---
+++ install_system.sh
@@ -0,0 +1,6 @@
+mkdir /usr/share/TorahBibleCodes
+cp -r * /usr/share/TorahBibleCodes/
+cd /usr/share/TorahBibleCodes/
+cp tbc /usr/bin/tbc
+chmod +x /usr/bin/tbc
+cd 
