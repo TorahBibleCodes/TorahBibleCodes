16b567cfc4ce953c04b5888e45fbe91af1a70c67
TorahBibleCodes <torahbiblecodes@gmail.com>
2021-12-02 11:32:44 +0200

    Commit mod_12GetLengthOfTextToSearch.py

---
+++ mod_12GetLengthOfTextToSearch.py
@@ -0,0 +1,20 @@
+## BEGIN FUNCTION () #12 - 
+
+def fn_GetLengthOfTextToSearch(L): ## PARAMETERS:  L (LIST OF LETTERS)
+    
+    ## TEST PRINT OUTPUT
+    print("\n")  ## PRINT SPACE
+    print("WITHIN FUNCTION:  BEGIN FUNCTION #12")
+          
+    ## DECLARE VARIABLES
+    LengthOfTextToSearch = len(L)
+    print("Length of Text to Search = ", LengthOfTextToSearch)
+
+    ## TEST PRINT OUTPUT
+    print("\n")  ## PRINT SPACE
+    print("WITHIN FUNCTION:  END FUNCTION #12")
+    
+    ## RETURN VARIABLES - LENGTH OF TEXT TO SEARCH
+    return(LengthOfTextToSearch)
+    
+## END FUNCTION () #12 - 
