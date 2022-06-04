3b7aedea185e0ddaf0f78eb67d626e09a0ef2fae
citrixrep <maxtvstream@live.com>
2022-06-04 02:49:11 +0200

    db

--- query.py
+++ query.py
@@ -39,6 +39,19 @@ def TextOfEN(query):
 						print(BLUE +'HE: ==> GE:'+str(dbitem[1])+' ==> '+ str(dbitem[2]) + END)
 
 
+def TextOfESMAKE(query):
+
+		ret = db.getTextOfES(' ')
+		if len(ret) !=0:
+				for dbitem in ret:
+						#print(dbitem[3]+'\n')
+						#print(YELLOW +'BOOK: ==> '+str(dbitem[5]) + END)
+						if query == 'en':
+							print(ORANGE +str(dbitem[4])+'\n' + END)
+						else:
+							print(ORANGE +str(dbitem[3])+'\n' + END)
+						#print(GREEN +'EN: ==> GE:'+str(dbitem[1])+' ==> '+ str(dbitem[4]) + END)
+						#print(BLUE +'HE: ==> GE:'+str(dbitem[1])+' ==> '+ str(dbitem[2]) + END)
 
 
 
@@ -51,6 +64,7 @@ parser = argparse.ArgumentParser(description="TBC")
 parser.add_argument('--book', action='store', type=str, required=False, help='query book (default: python3 query.py --book text_11IIkings)')
 parser.add_argument('--es', action='store', type=str, required=False, help='query spanish lang (default: python3 query.py --es caso)')
 parser.add_argument('--en', action='store', type=str, required=False, help='query english lang (default: python3 query.py --en case)')
+parser.add_argument('--make', action='store', type=str, required=False, help='make book (default: python3 query.py --make es)')
 args = parser.parse_args()
 
 if args.book:
@@ -58,6 +72,8 @@ if args.book:
 
 if args.es:
 	TextOfES(args.es)
+if args.make:
+	TextOfESMAKE(args.make)
 
 if args.en:
 	TextOfEN(args.en)
